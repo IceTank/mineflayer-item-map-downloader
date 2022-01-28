eebe4ac4f38a6b96a079a32019492a204a7d2f0d
IceTank <niklaslehle@gmail.com>
2022-01-28 01:08:06 +0100

    Add map saving as png

--- index.js
+++ index.js
@@ -2,40 +2,32 @@ const mc = require('mineflayer')
 const colors = require('./colors.json')
 const express = require('express')
 const app = express()
+const sharp = require('sharp')
+const { hexToByte } = require('./lib/util.js')
+const { promises: fs } = require('fs')
 
 const bot = mc.createBot({
   host: 'localhost',
-  username: 'archivebot',
-  port: 59898
+  username: 'archivebot'
 })
 
 const readline = require('readline')
 
 /** @type {Record<number, Buffer>} */
 const mapsData = {}
+/** @type {Record<number, Buffer>} */
+const maps = {}
 
 app.use(express.static('./public'))
 
 app.get('/maps', (req, res) => {
-  console.info('/maps')
-
-  const bufToString = (buffer) => {
-    return [...new Uint8Array(buffer)]
-  }
-  const entires = Object.entries(mapsData)
-  const newData = []
-  entires.forEach(e => {
-    newData.push({
+  const entires = Object.entries(maps)
+  res.json(entires.map(e => {
+    return {
       id: e[0],
-      data: bufToString(e[1])
-    })
-  })
-  res.json(newData)
-})
-
-app.get('/colors', (req, res) => {
-  console.info('/colors')
-  res.json(colors)
+      data: e[1].toString('base64')
+    }
+  }))
 })
 
 const serverInstance = app.listen(8080, () => {
@@ -43,31 +35,43 @@ const serverInstance = app.listen(8080, () => {
 })
 
 bot._client.on('packet', (data, meta) => {
-  // if (ignore.includes(meta.name)) return
   if (meta.name === 'map') {
     if (!(data.itemDamage in mapsData)) {
       mapsData[data.itemDamage] = new Buffer.from(data.data)
+      writeMap(data.data, data.itemDamage)
     }
   }
 })
 
+async function writeMap(data, mapId) {
+  const buf = new Buffer.from(data)
+  if (isNaN(Number(mapId))) throw new Error('Invalid map id')
+
+  const imgBuf = new Uint8ClampedArray(128 * 128 * 4)
+  for (let index = 0; index < imgBuf.byteLength; index += 4) {
+    const colorArr = hexToByte(colors[buf[index / 4]])
+    for (let k = 0; k < 4; k++) {
+      imgBuf[index + k] = colorArr[k]
+    }
+  }
+
+  await fs.mkdir('./images', { recursive: true })
+  const png = await sharp(imgBuf, {
+    raw: {
+      width: 128,
+      height: 128,
+      channels: 4
+    }
+  })
+    .png()
+    .toBuffer()
+  
+  maps[mapId] = png
+  fs.writeFile(`./images/map_${mapId.toString().padStart(6, '0')}.png`, png)
+}
+
 bot.once('spawn', () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  rl.on('SIGINT', () => {
-    bot.end()
-    rl.close()
-    serverInstance.close()
-  })
   console.info('Spawned')
-
-  bot.on('end', () => {
-    rl.close()
-    serverInstance.close()
-  })
 })
 
 bot.on('kicked', reason => console.info('Kicked for', reason))
--- public/index.js
+++ public/index.js
@@ -1,8 +1,3 @@
-let colors = null
-fetch('/colors').then(async data => {
-  colors = await data.json()
-}, (err) => console.error('Error fetching /colors', err))
-
 function loadAllMaps() {
   fetch('/maps').then(async data => {
     debugger
@@ -12,46 +7,19 @@ function loadAllMaps() {
     for (const map of data) {
       const mapData = map.data
       const mapId = map.id
-      const ele = document.createElement('canvas')
-      ele.width = 128
-      ele.height = 128
+      const canvas = document.createElement('canvas')
+      canvas.width = 128
+      canvas.height = 128
       const canvasId = 'canvas' + mapId
-      ele.id = canvasId
-      anchor.appendChild(ele)
-      console.info('Drawing', canvasId, mapData)
-      drawMap(canvasId, mapData)
+      canvas.id = canvasId
+      anchor.appendChild(canvas)
+      const image = new Image(128, 128)
+      image.src = 'data:image/png;base64,' + mapData
+      image.onload = () => {
+        console.info('Drawing', canvasId, mapData)
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(image, 0, 0)
+      }
     }
-    // drawMap('canvas1', data[0].data)
   }, (err) => console.error('Error fetching /maps', err))
-}
-
-function drawMap(canvasId, data) {
-  const c = document.getElementById(canvasId)
-  const canvasWidth = c.width;
-  const canvasHeight = c.height;
-  const ctx = c.getContext("2d");
-  const canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight)
-  console.info(ctx, data)
-
-  function drawPixel (x, y) {
-    const dataIndex = x + y * canvasWidth
-    const index = dataIndex * 4
-    const colorId = data[dataIndex]
-    const v = colors[colorId].replace('#', '')
-    canvasData.data[index + 0] = parseInt(v.slice(2, 4), 16)
-    canvasData.data[index + 1] = parseInt(v.slice(4, 6), 16)
-    canvasData.data[index + 2] = parseInt(v.slice(6, 8), 16)
-    canvasData.data[index + 3] = parseInt(v.slice(0, 2), 16)
-  }
-
-  function updateCanvas() {
-    ctx.putImageData(canvasData, 0, 0);
-  }
-
-  for (let x = 0; x < 128; x++) {
-    for (let y = 0; y < 128; y++) {
-      drawPixel(x, y)
-    }
-  }
-  updateCanvas()
 }
